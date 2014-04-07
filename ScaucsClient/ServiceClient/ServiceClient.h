57b4f7af82f475592806c1898da85f9059ece077
ccnyou <ccnyou@qq.com>
2014-04-07 08:44:34 +0800

    完整代码包

--- ScaucsClient/ServiceClient/ServiceClient.h
+++ ScaucsClient/ServiceClient/ServiceClient.h
@@ -17,6 +17,8 @@
 - (void)serviceClient:(ServiceClient *)client loginCompletedWithResult:(NSString *)result;
 - (void)serviceClient:(ServiceClient *)client loginFailedWithError:(NSError *)error;
 
+- (void)serviceClient:(ServiceClient *)client getCourseDetailCompletedWithResult:(NSArray *)result;
+- (void)serviceClient:(ServiceClient *)client getCourseDetailFailedWithError:(NSError *)error;
 @end
 
 
@@ -26,12 +28,21 @@
 
 //同步登陆
 - (NSString *)userLogin:(NSString *)userName andPswMD5:(NSString *)pswMD5;
+
 //同步 获取课程信息以及通知
 - (NSArray *)getMyCourseDetail:(NSString *)userName andSession:(NSString *)session;
+
 //通用同步方法调用
 + (NSData *)commonCall:(NSString *)methodName andParams:(NSDictionary *)params;
+
+
 //异步登陆接口
 - (void)userLoginAsync:(NSString *)userName andPswMD5:(NSString *)pswMD5;
 
+//异步获取课程信息以及通知
+- (void)getMyCourseDetailAsync:(NSString *)userName andSession:(NSString *)session;
+
+//MD5计算
++ (NSString *)getMD5:(NSString *)src;
 @end
 
--- ScaucsClient/ServiceClient/ServiceClient.m
+++ ScaucsClient/ServiceClient/ServiceClient.m
@@ -8,6 +8,7 @@
 
 #import "ServiceClient.h"
 #import "GDataXMLNode.h"
+#import <CommonCrypto/CommonDigest.h>
 
 #define SERVER_URL  @"http://wcf.scaucs.net/mainservice.svc"
 
@@ -41,6 +42,33 @@
     return self;
 }
 
+- (NSURLRequest *)requestWithMethodName:(NSString *)methodName andParams:(NSDictionary *)params
+{
+    NSArray* keys = [params allKeys];
+    
+    //构造请求
+    NSMutableString* bodyString = [NSMutableString stringWithString:@"<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">"];
+    
+    [bodyString appendFormat:@"<s:Body><%@ xmlns=\"http://tempuri.org/\">", methodName];
+    for (NSString* key in keys) {
+        NSString* value = [params valueForKey:key];
+        [bodyString appendFormat:@"<%@>%@</%@>", key, value, key];
+    }
+    [bodyString appendFormat:@"</%@></s:Body></s:Envelope>", methodName];
+    NSData* bodyData = [bodyString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    
+    NSString* serverUrlString = SERVER_URL;
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:serverUrlString]];
+    NSString* soapAction = [NSString stringWithFormat:@"\"http://tempuri.org/IMainService/%@\"", methodName];
+    [request addValue:soapAction forHTTPHeaderField:@"SOAPAction"];
+    [request setValue:@"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:bodyData];
+    [request setValue:[NSString stringWithFormat:@"%d", [bodyData length]] forHTTPHeaderField:@"Content-Length"];
+    
+    return request;
+}
 
 - (NSString *)userLogin:(NSString *)userName andPswMD5:(NSString *)pswMD5
 {
@@ -165,21 +193,11 @@
 - (void)userLoginAsync:(NSString *)userName andPswMD5:(NSString *)pswMD5
 {
     //构造请求
-    NSMutableString* bodyString = [NSMutableString stringWithString:@"<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">"];
-    [bodyString appendString:@"<s:Body><UserLogin xmlns=\"http://tempuri.org/\">"];
-    [bodyString appendFormat:@"<strUserName>%@</strUserName>", userName];
-    [bodyString appendFormat:@"<strPassWordMd5>%@</strPassWordMd5>", pswMD5];
-    [bodyString appendString:@"</UserLogin></s:Body></s:Envelope>"];
-    NSData* bodyData = [bodyString dataUsingEncoding:NSUTF8StringEncoding];
-    
-    NSString* serverUrlString = SERVER_URL;
-    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:serverUrlString]];
-    [request addValue:@"\"http://tempuri.org/IMainService/UserLogin\"" forHTTPHeaderField:@"SOAPAction"];
-    [request setValue:@"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-    [request setHTTPMethod:@"POST"];
-    [request setHTTPBody:bodyData];
-    [request setValue:[NSString stringWithFormat:@"%d", [bodyData length]] forHTTPHeaderField:@"Content-Length"];
-    
+    NSDictionary* params = @{
+                             @"strUserName" : userName,
+                             @"strPassWordMd5" : pswMD5
+                             };
+    NSURLRequest* request = [self requestWithMethodName:@"UserLogin" andParams:params];
     
     AFHTTPRequestOperation* operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     operation.responseSerializer = [AFHTTPResponseSerializer serializer];
@@ -211,6 +229,68 @@
     }];
     
     [operation start];
+}
+
+- (void)getMyCourseDetailAsync:(NSString *)userName andSession:(NSString *)session
+{
+    NSDictionary* params = @{
+                             @"strUserNumber" : userName,
+                             @"strSession" : session
+                             };
+    NSURLRequest* request = [self requestWithMethodName:@"GetMyCourseDetail" andParams:params];
+    
+    AFHTTPRequestOperation* operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if ([_delegate respondsToSelector:@selector(serviceClient:getCourseDetailCompletedWithResult:)]) {
+
+            NSData* xmlData = responseObject;
+            GDataXMLDocument* doc = [[GDataXMLDocument alloc] initWithData:xmlData options:0 error:nil];
+            GDataXMLElement* rootElement = [doc rootElement];
+            GDataXMLNode* node = [rootElement childAtIndex:0];
+            node = [node childAtIndex:0];
+            node = [node childAtIndex:0];
+            
+            NSMutableArray* results = [[NSMutableArray alloc] init];
+            NSArray* array = [node children];
+            for (GDataXMLElement* objNode in array) {
+                NSAssert([[objNode name] isEqualToString:@"a:ArrayOfstring"], @"貌似出错了，返回数据不是一个字符串数组");
+                
+                NSMutableArray* strings = [[NSMutableArray alloc] initWithCapacity:5];
+                NSArray* elems = [objNode children];
+                for (GDataXMLElement* elem in elems) {
+                    [strings addObject:[elem stringValue]];
+                }
+                
+                if ([strings count] > 0) {
+                    [results addObject:strings];
+                }
+            }
+            
+            [_delegate serviceClient:self getCourseDetailCompletedWithResult:results];
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if ([_delegate respondsToSelector:@selector(serviceClient:getCourseDetailFailedWithError:)]) {
+            [_delegate serviceClient:self getCourseDetailFailedWithError:error];
+        }
+    }];
+    
+    [operation start];
+}
+
++ (NSString *)getMD5:(NSString *)src
+{
+    const char* cstr = [src UTF8String];
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(cstr, strlen(cstr), digest);
+    
+    NSMutableString* result = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [result appendFormat:@"%02x", digest[i]];
+    }
+    
+    return result;
 }
 
 @end
